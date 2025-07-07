8c8164128ce71cb5d164d1a8c6a699c25501293b
Peter Ovchyn <peter@boarlabs.xyz>
2025-07-07 22:51:30 +0200

    feat: Update ESLint to allow underscore-prefixed unused variables
    - Update @typescript-eslint/no-unused-vars rule to ignore underscore-prefixed variables
    - Add argsIgnorePattern, varsIgnorePattern, and caughtErrorsIgnorePattern for ^_ pattern
    - Simplify Step::execute parameter to use _context instead of ESLint disable comment
    - Remove unnecessary ESLint disable comment and variable assignment

--- src/flow/step.ts
+++ src/flow/step.ts
@@ -36,10 +36,8 @@ export class Step implements IStep {
     return this.nextStepId;
   }
 
-  public execute(context: IContext): Promise<boolean> {
+  public execute(_: IContext): Promise<boolean> {
     // Context is available for future use by step implementations
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const _context = context;
     this.logger.info(this.message);
     return Promise.resolve(true);
   }
